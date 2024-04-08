72c09df09392f74e40c833019d1f4f26ec8e87ae
Cao Minh Duc <caominhduc31081@gmail.com>
2024-04-08 18:23:50 +0700

    Initiial Commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///locations.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(200))
+    
+
+@app.route('/')
+def index():
+    locations = Location.query.all()
+    return render_template('index.html', locations=locations)
+
+@app.route('/add', methods=['POST'])
+def add_location():
+    name = request.form['name']
+    description = request.form['description']
+    new_location = Location(name=name, description=description)
+    db.session.add(new_location)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update_location(id):
+    location = Location.query.get_or_404(id)
+    if request.method == 'POST':
+        location.name = request.form['name']
+        location.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update.html', location=location)
+
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete_location(id):
+    location = Location.query.get_or_404(id)
+    db.session.delete(location)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
